68af2740d3cf5ee693d0ed6b0b9d14b64972b4ee
Ben Levin <benjaminwlevin@gmail.com>
2017-08-20 16:45:29 -0500

    fixed order

--- Controllers/QuoteController.cs
+++ Controllers/QuoteController.cs
@@ -39,7 +39,7 @@ using Newtonsoft.Json;
         [Route("/quotes/")]
         public IActionResult Quotes()
         {
-            List<Dictionary<string, object>> AllQuotes = DbConnector.Query("SELECT * FROM quotes");
+            List<Dictionary<string, object>> AllQuotes = DbConnector.Query("SELECT * FROM quotes ORDER BY created_at DESC");
             Console.WriteLine(AllQuotes[0]["id"]);
             ViewBag.Quotes=(List<Dictionary<string,object>>)AllQuotes;
             return View("index");
@@ -71,3 +71,56 @@ using Newtonsoft.Json;
     }
 }
 
+//from solution
+
+// using Microsoft.AspNetCore.Mvc;
+// using System.Linq;
+// using DbConnection;
+// using System;
+
+// namespace QuotingDojo {
+//     public class QuoteController : Controller {
+//         [HttpGet]
+//         [Route("")]
+//         public IActionResult Index() {
+//             if(TempData["Error"] != null){
+//                 ViewBag.Error = TempData["Error"];
+//             }
+//             return View();
+//         }
+
+//         [HttpPost]
+//         [Route("/quotes")]
+//         public IActionResult Create(string name, string content){
+//             if(name == "" || content == ""){
+//                 TempData["Error"] = "Neither field should be empty!";
+//                 return RedirectToAction("Index");
+//             }
+//             //Add the quote to the database
+//             string query = $"INSERT INTO quotes (content, poster, created_at, updated_at) VALUES ('{content}', '{name}', NOW(), NOW());";
+//             DbConnector.Execute(query);
+//             return RedirectToAction("Quotes");    
+//         }
+
+//         [HttpGet]
+//         [Route("/quotes")]
+//         public IActionResult Quotes(){
+//             //Get all quotes
+//             string query = "SELECT * FROM quotes";
+//             var quotes = DbConnector.Query(query);
+
+//             //Sort them by created time descending
+//             quotes = quotes.OrderByDescending((quote) => quote["created_at"]).ToList();
+
+//             //Format all of the dates
+//             foreach(var quote in quotes){
+//                 DateTime created = (DateTime)quote["created_at"];
+//                 string formatted_created = String.Format("{0:h:mm tt MMMM d yyyy}", created);
+//                 quote["created_at"] = formatted_created;
+//             }
+
+//             ViewBag.Quotes = quotes;
+//             return View();
+//         }
+//     }
+// }
